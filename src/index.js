820ec9911502d47a64f5fc364318db08cf78efc5
Tyler Cooke <tylerstouchpad@gmail.com>
2019-05-07 10:01:58 -0400

    adds redux library for createStore

--- src/index.js
+++ src/index.js
@@ -3,9 +3,61 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import {createStore} from 'redux';
 import initialState from './base.json';
-console.log(initialState)
+console.log(initialState);
+
+// ==========================================================================
+// STATE 
+
+
+
+
+
+
+// ==========================================================================
+// ACTION + ACTION CREATORS
+const ACTION_CATCH = 'catch';
+
+function catchCard(id) {
+    return {
+    type: ACTION_CATCH,
+    payload: {
+        id,
+        }
+    }
+}
+window.catchCard = catchCard;
+
+
+// ==========================================================================
+// REDUCER 
+
+function cards(state=initialState, action={type: ''}) { 
+    console.log(`cards got called with ${action.payload}`);
+    switch(action.type) {
+        case ACTION_CATCH:
+        console.log(`cards got called with ${action.payload.id}`);
+            // find the card, set it to "caught"
+        break;
+
+        default:
+            return state;
+        break;
+    }
+}
+
+
+// ==========================================================================
+// STORE
+const store = createStore(cards);
+window.store = store;
+
+
+
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
